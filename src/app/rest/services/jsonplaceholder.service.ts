9b4b1a1773b7568969ac1679509f7e768b4599d7
Teoh Kheng Hong <tkhenghong@gmail.com>
2021-01-22 16:00:25 +0800

    Added commens on the REST APIs

--- src/app/rest/services/jsonplaceholder.service.ts
+++ src/app/rest/services/jsonplaceholder.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CoreService } from '../../services';
 import { Post, PostComment } from '../../models';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class JsonplaceholderService {
@@ -12,15 +13,36 @@ export class JsonplaceholderService {
               private coreService: CoreService) {
   }
 
+  /**
+   * Get all posts.
+   * NOTE: No pagination.
+   */
   getPost(): Observable<Post[]> {
     return this.http.get<Post[]>(`${this.restURL}/posts`);
   }
 
+  /**
+   * Get info about a post.
+   * @param postId: ID of the post.
+   */
   getPostInfo(postId: number): Observable<Post> {
     return this.http.get<Post>(`${this.restURL}/posts/${postId}`);
   }
 
+  /**
+   * Get the comments of the post.
+   * NOTE: No pagination.
+   * @param postId: ID of the post.
+   */
   getPostComments(postId: number): Observable<PostComment[]> {
     return this.http.get<PostComment[]>(`${this.restURL}/comments?postId=${postId}`);
+  }
+
+  /**
+   * NOTE: This should be the API that search the DB in the backend to find out the post
+   * @param searchText: Search keyword.
+   */
+  searchPost(searchText: string): void /*Observable<SearchResult[]>*/ {
+    // Do nothing.
   }
 }
